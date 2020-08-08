6b3001734ea156b96494d94aac2685fa9e01705e
simondlevy <simon.d.levy@gmail.com>
2020-08-07 20:06:47 -0400

    Reading bytes

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+PORT = '/dev/ttyUSB0'
+
 import serial
 
+port = serial.Serial(port=PORT, baudrate=12000000, rtscts=True)
+
+port.write('R\n'.encode())
+
+port.write('E+\n'.encode())
+
+port.write('!E0\n'.encode())
+
+while True:
+    print(port.read())
 
