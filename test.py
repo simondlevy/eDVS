5a0c2a96aa2e878f6bb6ca847f27fbe09564b124
simondlevy <simon.d.levy@gmail.com>
2020-08-08 23:07:45 -0400

    More thread communication

--- test.py
+++ test.py
@@ -3,7 +3,7 @@
 PORT = '/dev/ttyUSB0'
 
 import serial
-from time import sleep
+from time import time, sleep
 import cv2
 import numpy as np
 from threading import Thread
@@ -13,7 +13,7 @@ def send(port, cmd):
     port.write((cmd + '\n').encode())
     sleep(.01)
 
-def read_sensor(image):
+def read_sensor(image, flags):
 
     port = serial.Serial(port=PORT, baudrate=12000000, rtscts=True)
 
@@ -29,7 +29,7 @@ def read_sensor(image):
     x      = None
     second = False
 
-    while True:
+    while flags[0]:
 
         v = ord(port.read()) & 0b01111111
 
@@ -41,11 +41,15 @@ def read_sensor(image):
 
         second = not second
 
+    print('done')
+
 def main():
 
     image = np.zeros((128,128))
 
-    thread = Thread(target=read_sensor, args = (image,))
+    flags = [True]
+
+    thread = Thread(target=read_sensor, args = (image,flags))
     thread.daemon = True
     thread.start()
 
@@ -58,6 +62,9 @@ def main():
 
     cv2.destroyAllWindows()
 
+    flags[0] = False
+
+    thread.join()
 
 if __name__ == '__main__':
 
