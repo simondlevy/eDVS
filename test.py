50f3f3cce716df1fb21ba3b3941f2e6c6a0b795d
simondlevy <simon.d.levy@gmail.com>
2020-08-08 22:47:37 -0400

    Begin OpenCV display work

--- test.py
+++ test.py
@@ -4,14 +4,16 @@ PORT = '/dev/ttyUSB0'
 
 import serial
 from time import sleep
+import cv2
+import numpy as np
+from threading import Thread
 
 def send(port, cmd):
 
     port.write((cmd + '\n').encode())
     sleep(.01)
 
-
-def main():
+def read_sensor():
 
     port = serial.Serial(port=PORT, baudrate=12000000, rtscts=True)
 
@@ -37,6 +39,25 @@ def main():
             x = v
 
         second = not second
+
+def main():
+
+    #thread = Thread(target=read_sensor, args = (plotter,))
+    thread = Thread(target=read_sensor)
+    thread.daemon = True
+    thread.start()
+
+    while(True):
+
+        image = np.zeros((128,128))
+
+        # Display the resulting image
+        cv2.imshow('image', cv2.resize(image, ((512,512))))
+        if cv2.waitKey(1) == 27:
+            break
+
+    cv2.destroyAllWindows()
+
 
 if __name__ == '__main__':
 
