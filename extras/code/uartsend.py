55a9a06f768f907f82a8b92f7d9533ea8f57dc7d
simondlevy <simon.d.levy@gmail.com>
2020-08-18 03:52:44 -0400

    Sending alphabet

--- extras/code/uartsend.py
+++ extras/code/uartsend.py
@@ -4,11 +4,15 @@ import serial
 
 port = serial.Serial('/dev/ttyUSB1', 115200)
 
+c = 0
+
 while True:
 
     try:
 
-        port.write('A'.encode())
+        port.write(chr(65+c).encode()) # 65 = 'A'
+
+        c =  (c+1) % 26
 
     except KeyboardInterrupt:
 
