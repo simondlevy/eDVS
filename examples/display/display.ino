cc072bf70b288e32aacd94f696c51744d77ddf24
simondlevy <simon.d.levy@gmail.com>
2020-10-09 23:13:50 -0400

    Drawing on OLED without erasing

--- examples/display/display.ino
+++ examples/display/display.ino
@@ -7,6 +7,29 @@ MIT License
 */
 
 #include "eDVS.h"
+#include <OLED_GFX.h>
+
+// OLED Pins
+static const uint8_t CS  = 10;
+static const uint8_t DC  = 9;
+static const uint8_t RST = 8;
+
+OLED_GFX oled(CS, DC, RST);
+
+class SparseMatrix {
+
+    private:
+
+        typedef struct {
+
+            uint8_t x;
+            uint8_t y;
+        } coords;
+
+        uint32_t a[128][128];
+
+        uint8_t t[128*128];
+};
 
 static eDVS edvs(&Serial1);
 
@@ -20,15 +43,25 @@ void serialEvent1(void)
 void setup(void)
 {
     Serial.begin(115200);
+
     edvs.begin(2000000);
+
+    oled.begin();
+
+    oled.Clear_Screen();
+
 }
 
 void loop(void)
 {
+
     while (edvs.hasNext()) {
+
         eDVS::event_t e;
         edvs.next(e);
-        Serial.printf("x=%3d y=%3d p=%+d t=%d\n", e.x, e.y, e.p, e.t);
+        oled.Draw_Pixel(e.x,e.y);
+        oled.Set_Color(e.p == -1 ? OLED_GFX::GREEN : OLED_GFX::RED);
     }
+
     delay(1);
 }
