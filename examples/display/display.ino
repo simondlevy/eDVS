dc6b8bb9badeb3efeef4820a460e8f69e1005c3c
simondlevy <simon.d.levy@gmail.com>
2020-10-11 15:23:45 -0400

    Cleanup

--- examples/display/display.ino
+++ examples/display/display.ino
@@ -14,14 +14,9 @@ static const uint8_t CS  = 10;
 static const uint8_t DC  = 9;
 static const uint8_t RST = 8;
 
-typedef struct {
-    uint8_t x;
-    uint8_t y;
-    uint32_t t;
-} event_t;
-
+// Queue of old events for erasing
 static const uint16_t QSIZE = 1000;
-static event_t queue[QSIZE];
+static eDVS::event_t queue[QSIZE];
 static uint16_t qpos;
 
 OLED_GFX oled(CS, DC, RST);
@@ -50,25 +45,24 @@ void setup(void)
 
 void loop(void)
 {
-    // Get events from DVS, storing times and pixels
+    // Get events from DVS
     if (edvs.hasNext()) {
 
+        // Display event
         eDVS::event_t e;
         edvs.next(e);
         oled.Set_Color(e.p == -1 ? OLED_GFX::GREEN : OLED_GFX::RED);
         oled.Draw_Pixel(e.y,e.x);
 
+        // Store event in queue
         queue[qpos].x = e.x;
         queue[qpos].y = e.y;
-        queue[qpos].t = millis();
         qpos = (qpos + 1) % QSIZE;
 
     }
 
-    event_t e2 = queue[(qpos+1)%QSIZE];
-    if (e2.t > 0 /*&& (millis() - e2.t) > 1*/) {
-        oled.Set_Color(OLED_GFX::BLACK);
-        oled.Draw_Pixel(e2.y,e2.x);
-        e2.t = 0;
-    }
+    // Remove pixel for oldest event in queue
+    eDVS::event_t e2 = queue[(qpos+1)%QSIZE];
+    oled.Set_Color(OLED_GFX::BLACK);
+    oled.Draw_Pixel(e2.y,e2.x);
 }
