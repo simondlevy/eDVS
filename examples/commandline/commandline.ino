4d5e15bc86e6306989d6e8e0f2d3ac7939054035
simondlevy <simon.d.levy@gmail.com>
2023-03-10 16:17:03 -0500

    Separate examples for command-line and relay

--- examples/commandline/commandline.ino
+++ examples/commandline/commandline.ino
@@ -12,7 +12,7 @@ static const uint8_t TX_PIN = 14;
 
 void setup(void)
 {
-    Serial.begin(2000000);
+    Serial.begin(115200);
 
     // ESP32
     Serial1.begin(2000000, SERIAL_8N1, RX_PIN, TX_PIN);
