d5e5391b724228a9ce0f9d94939a2afda3d5cf55
simondlevy <simon.d.levy@gmail.com>
2023-03-10 16:37:35 -0500

    =More ESP-NOW sketch

--- examples/esp32sender/esp32sender.ino
+++ examples/esp32sender/esp32sender.ino
@@ -6,12 +6,25 @@ Copyright (C) 2023 Simon D. Levy
 MIT License
 */
 
+#include <esp_now.h>
+#include <WiFi.h>
+
 #include "edvs.h"
+
+static const uint8_t ESP_RECEIVER_ADDRESS[] = {0xAC, 0x0B, 0xFB, 0x6F, 0x6C, 0x04};
 
 static const uint8_t RX_PIN = 4;
 static const uint8_t TX_PIN = 14;
 
 static eDVS edvs(Serial1);
+
+static void reportForever(const char * msg)
+{
+    while (true) {
+        Serial.println(msg);
+        delay(500);
+    }
+}
 
 void setup(void)
 {
@@ -20,6 +33,22 @@ void setup(void)
     Serial1.begin(2000000, SERIAL_8N1, RX_PIN, TX_PIN);
 
     edvs.begin();
+
+    WiFi.mode(WIFI_STA);
+
+    if (esp_now_init() != ESP_OK) {
+        reportForever("Error initializing ESP-NOW");
+    }
+
+    static esp_now_peer_info_t peerInfo;
+
+    memcpy(peerInfo.peer_addr, ESP_RECEIVER_ADDRESS, 6);
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+
+    if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+        reportForever("Failed to add peer");
+    }
 }
 
 void loop(void)
