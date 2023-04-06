d895881cb4ebafa3e9546d5d516cb9b11a69d0e3
simondlevy <simon.d.levy@gmail.com>
2023-04-06 14:08:56 -0400

    =More Knoise filtering

--- python/filters/knoise.py
+++ python/filters/knoise.py
@@ -16,15 +16,44 @@ class ONFilter:
 
     DEFAULT_TIMESTAMP = 0
 
-    def __init__(self, delta_t_msec=1, supporters=1):
+    def __init__(self, sx=128, sy=128, delta_t_msec=1, supporters=1):
 
-        self.delta_t_msec = delta_t_msec
+        self.sx = sx
+        self.sy = sy
+
+        self.delta_t_usec = 1000 * delta_t_msec
         self.supporters = supporters
 
+        self.last_row_ts = self.DEFAULT_TIMESTAMP * np.ones(sy)
+        self.last_col_ts = self.DEFAULT_TIMESTAMP * np.ones(sx)
+
+        self.last_x_bye_row = np.zeros(128)
 
     def check(self, e):
         '''
         Returns True if event e passes filter, False otherwise
         '''
 
-        return True
+        # assume all edge events are noise and filter out
+        if e.x <= 0 or e.y <= 0 or e.x >= self.sx - 1 or e.y >= self.sy - 1:
+            return False
+
+
+        # first check rows around us, if any adjancent row has event then
+        # filter in
+        for y in range(-self.supporters, self.supporters+1):
+            if (self.last_row_ts[e.y + y] != self.DEFAULT_TIMESTAMP
+                and e.timestamp - self.last_row_ts[e.y + y] < self.dt_usec
+                and abs(self.last_x_by_row[e.y + y] - e.x) <= 1):
+                # if there was event (ts!=DEFAULT_TIMESTAMP), and the timestamp
+                # is recent enough, and the column was adjacent, then filter in
+               return True
+
+        # now do same for columns
+        for x in range(-self.supporters, self.supporters+1):
+            if (self.last_col_ts[e.x + x] != self.DEFAULT_TIMESTAMP and
+                e.timestamp - self.last_col_ts[e.x + x] < self.dt_usec and
+                abs(self.last_y_by_col[e.x + x] - e.y) <= 1):
+                return True
+
+        return False
