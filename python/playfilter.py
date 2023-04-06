0035f4ae1e8bd678e0f77c51a9f47bd38194492a
simondlevy <simon.d.levy@gmail.com>
2023-04-06 13:39:12 -0400

    Moved STCF code to dvsnoise.py

--- python/playfilter.py
+++ python/playfilter.py
@@ -13,7 +13,7 @@ import argparse
 import cv2
 from time import time
 
-from filters import SpatioTemporalCorrelationFilter
+from filters.dvsnoise import SpatioTemporalCorrelationFilter
 
 def main():
 
