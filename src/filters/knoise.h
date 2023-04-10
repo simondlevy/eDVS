2f4b757418808700491f759441a1eb1a8f4f71d2
simondlevy <simon.d.levy@gmail.com>
2023-04-10 18:51:00 -0400

    More knoise filter work

--- src/filters/knoise.h
+++ src/filters/knoise.h
@@ -25,15 +25,38 @@ class OrderNbackgroundActivityFilter {
                 const uint32_t dt_msec=10,
                 const uint8_t supporters=10)
         {
-            (void)last_timestamp;
-            (void)sx;
-            (void)sy;
-            (void)dt_msec;
-            (void)supporters;
+            _sx = sx;
+            _sy = sy;
+            _supporters = supporters;
+
+            _dt_usec = 1000 * dt_msec;
+
+            for (uint8_t k=0; k<128; ++k) {
+                _last_row_ts[k] = DEFAULT_TIMESTAMP;
+                _last_col_ts[k] = DEFAULT_TIMESTAMP;
+                _last_x_by_row[k] = 0;
+                _last_y_by_col[k] = 0;
+            }
         }
 
         bool check(const EDVS::event_t & e)
         {
             return true;
         }
-};
+
+    private:
+
+        static const uint32_t DEFAULT_TIMESTAMP = 0;
+
+        uint8_t _sx;
+        uint8_t _sy;
+        uint32_t _dt_usec;
+        uint8_t _supporters;
+
+        uint32_t _last_row_ts[128];
+        uint32_t _last_col_ts[128];
+
+        uint8_t _last_x_by_row[128];
+        uint8_t _last_y_by_col[128];
+
+ };
