0b434ebcf349faeb837112d79c2890974e06fffc
simondlevy <simon.d.levy@gmail.com>
2023-04-11 19:42:30 -0400

    Added nrf24 examples

--- examples/nrf24/sender/sender.ino
+++ examples/nrf24/sender/sender.ino
@@ -6,14 +6,33 @@ Copyright (C) 2023 Simon D. Levy
 MIT License
 */
 
+#include <RF24.h>
+
 #include "edvs.h"
 #include "filters/knoise.h"
 #include "filters/passthru.h"
+
+static const uint8_t CE_PIN = 9;
+
+static const uint32_t RADIO_FREQ = 18000000;
+
+static const byte RADIO_ADDRESS[6] = "00001";
+
+static RF24 radio(CE_PIN, SS, RADIO_FREQ); // use builtin chip-select pin SS
 
 static EDVS edvs; 
 
 static OrderNbackgroundActivityFilter filter;
 //static PassThruFilter filter;
+
+static void startRadio(void)
+{
+    radio.begin();
+    radio.setDataRate(RF24_2MBPS );
+    radio.openWritingPipe(RADIO_ADDRESS);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.stopListening();
+}
 
 void serialEvent1(void)
 {
@@ -36,9 +55,16 @@ void setup(void)
 {
     Serial.begin(115200);
 
+    startRadio();
+
     edvs.begin(Serial1);
 }
 
 void loop(void)
 {
+    uint8_t buf[32] = {};
+    for (uint8_t k=0; k<sizeof(buf); ++k) {
+        buf[k] = k;
+    }
+    radio.write(buf, sizeof(buf));
 }
