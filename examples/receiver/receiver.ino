8bf31c6207e8bdd8d38f98b0946de31453323972
simondlevy <simon.d.levy@gmail.com>
2023-04-11 19:37:51 -0400

    Begin nRF24 work

---
+++ examples/receiver/receiver.ino
@@ -0,0 +1,44 @@
+#include <RF24.h>
+
+static const uint8_t CE_PIN = 7;
+static const uint8_t CS_PIN = 8;
+
+static RF24 radio(CE_PIN, CS_PIN, 18000000);
+
+static const byte ADDRESS[6] = "00001";
+
+void setup() 
+{
+    Serial.begin(115200);
+
+    radio.begin();
+    radio.setDataRate( RF24_2MBPS );
+    radio.openReadingPipe(0, ADDRESS);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.startListening();
+}
+
+void loop() 
+{
+    static uint32_t count;
+    static uint8_t buf[32] = {};
+
+    while (radio.available()) {
+        radio.read(buf, sizeof(buf));
+        count += sizeof(buf);
+    }
+
+    static uint32_t usec_prev;
+    auto usec = micros();
+    if (usec - usec_prev > 1000000) {
+        if (usec_prev > 0) {
+            printf("%06d bytes / second:", count);
+            for (uint8_t k=0; k<sizeof(buf); ++k) {
+                printf(" %02d", buf[k]);
+            }
+            printf("\n");
+        }
+        usec_prev = usec;
+        count = 0;
+    }
+}
